a21acef1e725224b969c04141f4ea22c5e6fb10d
amenkes319 <amenkes319@gmail.com>
2020-10-01 01:35:45 -0400

    selection screen smaller

--- src/main/java/controllers/SelectionController.java
+++ src/main/java/controllers/SelectionController.java
@@ -40,60 +40,60 @@ public class SelectionController
 	
 	private void init()
 	{
-		this.topHBox.prefWidthProperty().bind(this.parentVBox.widthProperty());
-		this.topHBox.prefHeightProperty().bind(this.parentVBox.heightProperty());
+		topHBox.prefWidthProperty().bind(parentVBox.widthProperty());
+		topHBox.prefHeightProperty().bind(parentVBox.heightProperty());
 
-		this.midHBox.prefWidthProperty().bind(this.parentVBox.widthProperty());
-		this.midHBox.prefHeightProperty().bind(this.parentVBox.heightProperty());
+		midHBox.prefWidthProperty().bind(parentVBox.widthProperty());
+		midHBox.prefHeightProperty().bind(parentVBox.heightProperty());
 		
-		this.bottomHBox.prefWidthProperty().bind(this.parentVBox.widthProperty());
-		this.bottomHBox.prefHeightProperty().bind(this.parentVBox.heightProperty());
+		bottomHBox.prefWidthProperty().bind(parentVBox.widthProperty());
+		bottomHBox.prefHeightProperty().bind(parentVBox.heightProperty());
 		
-		this.clubBtn.setOnAction(e -> Global.makerController.changeToScene(Map.CLUBHOUSE));
-		this.clubBtn.prefWidthProperty().bind(this.parentVBox.widthProperty());
-		this.clubBtn.prefHeightProperty().bind(this.parentVBox.heightProperty());
-		this.clubBtn.setGraphic(new ImageView("/main/resources/Maps/Clubhouse.png"));
-		this.clubBtn.getStylesheets().add("/main/css/Selection.css");
-		this.clubBtn.getGraphic().prefWidth(this.clubBtn.getWidth());
-		this.clubBtn.getGraphic().prefHeight(this.clubBtn.getHeight());
+		clubBtn.setOnAction(e -> Global.makerController.changeToScene(Map.CLUBHOUSE));
+		clubBtn.prefWidthProperty().bind(parentVBox.widthProperty());
+		clubBtn.prefHeightProperty().bind(parentVBox.heightProperty());
+		clubBtn.setGraphic(new ImageView("/main/resources/Maps/Clubhouse.png"));
+		clubBtn.getStylesheets().add("/main/css/Selection.css");
+		clubBtn.getGraphic().prefWidth(clubBtn.getWidth());
+		clubBtn.getGraphic().prefHeight(clubBtn.getHeight());
 		
-		this.coastBtn.setOnAction(e -> Global.makerController.changeToScene(Map.COASTLINE));
-		this.coastBtn.getStylesheets().add("/main/css/Selection.css");
-		this.coastBtn.setGraphic(new ImageView("/main/resources/Maps/Coastline.png"));
-		this.coastBtn.prefWidthProperty().bind(this.parentVBox.widthProperty());
-		this.coastBtn.prefHeightProperty().bind(this.parentVBox.heightProperty());
+		coastBtn.setOnAction(e -> Global.makerController.changeToScene(Map.COASTLINE));
+		coastBtn.getStylesheets().add("/main/css/Selection.css");
+		coastBtn.setGraphic(new ImageView("/main/resources/Maps/Coastline.png"));
+		coastBtn.prefWidthProperty().bind(parentVBox.widthProperty());
+		coastBtn.prefHeightProperty().bind(parentVBox.heightProperty());
 		
-		this.consulateBtn.setOnAction(e -> Global.makerController.changeToScene(Map.CONSULATE));
-		this.consulateBtn.getStylesheets().add("/main/css/Selection.css");
-		this.consulateBtn.setGraphic(new ImageView("/main/resources/Maps/Consulate.png"));
-		this.consulateBtn.prefWidthProperty().bind(this.parentVBox.widthProperty());
-		this.consulateBtn.prefHeightProperty().bind(this.parentVBox.heightProperty());
+		consulateBtn.setOnAction(e -> Global.makerController.changeToScene(Map.CONSULATE));
+		consulateBtn.getStylesheets().add("/main/css/Selection.css");
+		consulateBtn.setGraphic(new ImageView("/main/resources/Maps/Consulate.png"));
+		consulateBtn.prefWidthProperty().bind(parentVBox.widthProperty());
+		consulateBtn.prefHeightProperty().bind(parentVBox.heightProperty());
 		
-		this.kafeBtn.setOnAction(e -> Global.makerController.changeToScene(Map.KAFE));
-		this.kafeBtn.getStylesheets().add("/main/css/Selection.css");
-		this.kafeBtn.setGraphic(new ImageView("/main/resources/Maps/Kafe.png"));
-		this.kafeBtn.prefWidthProperty().bind(this.parentVBox.widthProperty());
-		this.kafeBtn.prefHeightProperty().bind(this.parentVBox.heightProperty());
+		kafeBtn.setOnAction(e -> Global.makerController.changeToScene(Map.KAFE));
+		kafeBtn.getStylesheets().add("/main/css/Selection.css");
+		kafeBtn.setGraphic(new ImageView("/main/resources/Maps/Kafe.png"));
+		kafeBtn.prefWidthProperty().bind(parentVBox.widthProperty());
+		kafeBtn.prefHeightProperty().bind(parentVBox.heightProperty());
 		
-		this.oregonBtn.setOnAction(e -> Global.makerController.changeToScene(Map.OREGON));
-		this.oregonBtn.getStylesheets().add("/main/css/Selection.css");
-		this.oregonBtn.setGraphic(new ImageView("/main/resources/Maps/Oregon.png"));
-		this.oregonBtn.prefWidthProperty().bind(this.parentVBox.widthProperty());
-		this.oregonBtn.prefHeightProperty().bind(this.parentVBox.heightProperty());
+		oregonBtn.setOnAction(e -> Global.makerController.changeToScene(Map.OREGON));
+		oregonBtn.getStylesheets().add("/main/css/Selection.css");
+		oregonBtn.setGraphic(new ImageView("/main/resources/Maps/Oregon.png"));
+		oregonBtn.prefWidthProperty().bind(parentVBox.widthProperty());
+		oregonBtn.prefHeightProperty().bind(parentVBox.heightProperty());
 		
-		this.themeBtn.setOnAction(e -> Global.makerController.changeToScene(Map.THEMEPARK));
-		this.themeBtn.getStylesheets().add("/main/css/Selection.css");
-		this.themeBtn.setGraphic(new ImageView("/main/resources/Maps/Themepark.png"));
-		this.themeBtn.prefWidthProperty().bind(this.parentVBox.widthProperty());
-		this.themeBtn.prefHeightProperty().bind(this.parentVBox.heightProperty());
+		themeBtn.setOnAction(e -> Global.makerController.changeToScene(Map.THEMEPARK));
+		themeBtn.getStylesheets().add("/main/css/Selection.css");
+		themeBtn.setGraphic(new ImageView("/main/resources/Maps/Themepark.png"));
+		themeBtn.prefWidthProperty().bind(parentVBox.widthProperty());
+		themeBtn.prefHeightProperty().bind(parentVBox.heightProperty());
 		
-		this.villaBtn.setOnAction(e -> Global.makerController.changeToScene(Map.VILLA));
-		this.villaBtn.getStylesheets().add("/main/css/Selection.css");
-		this.villaBtn.setGraphic(new ImageView("/main/resources/Maps/Villa.png"));
-//		this.villaBtn.prefWidth(this.oregonBtn.getWidth());
-//		this.villaBtn.prefHeight(this.oregonBtn.getHeight());
-		this.villaBtn.prefWidthProperty().bind(this.parentVBox.widthProperty());
-		this.villaBtn.prefHeightProperty().bind(this.parentVBox.heightProperty());
+		villaBtn.setOnAction(e -> Global.makerController.changeToScene(Map.VILLA));
+		villaBtn.getStylesheets().add("/main/css/Selection.css");
+		villaBtn.setGraphic(new ImageView("/main/resources/Maps/Villa.png"));
+//		villaBtn.prefWidth(oregonBtn.getWidth());
+//		villaBtn.prefHeight(oregonBtn.getHeight());
+		villaBtn.prefWidthProperty().bind(parentVBox.widthProperty());
+		villaBtn.prefHeightProperty().bind(parentVBox.heightProperty());
 		
 	}
 	
